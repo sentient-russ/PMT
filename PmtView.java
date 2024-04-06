364a50a53b9f859ef34acbd0b9a260a12e26a59d
Ezeobiekene <ezeobiekene7@gmail.com>
2024-04-06 15:36:42 -0400

    added the add New Project screen

--- PmtView.java
+++ PmtView.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.DefaultTableCellRenderer;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 
@@ -30,6 +32,15 @@ public class PmtView extends JFrame{
         add.setForeground(Color.WHITE);
         add.setBorderPainted(false);
         add.setFocusPainted(false);
+        add.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+              //add button functionality goes here
+                addNewPage();
+                panel.setVisible(false);
+            }
+        });
         panel.add(add);
 
         JButton update = new JButton("Update");
@@ -41,6 +52,13 @@ public class PmtView extends JFrame{
         update.setForeground(Color.WHITE);
         update.setBorderPainted(false);
         update.setFocusPainted(false);
+        update.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //update button functionality goes here
+            }
+        });
         panel.add(update);
 
         JButton delete = new JButton("Delete");
@@ -52,6 +70,13 @@ public class PmtView extends JFrame{
         delete.setForeground(Color.WHITE);
         delete.setBorderPainted(false);
         delete.setFocusPainted(false);
+        delete.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //delete button functionality goes here
+            }
+        });
         panel.add(delete);
 
         JButton exit = new JButton("X");
@@ -63,6 +88,14 @@ public class PmtView extends JFrame{
         exit.setForeground(Color.WHITE);
         exit.setBorderPainted(false);
         exit.setFocusPainted(false);
+        exit.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //exit button functionality goes here
+                majorFrame.dispose();
+            }
+        });
         panel.add(exit);
 
         JLabel logo = new JLabel("P.M.T Logo");
@@ -86,7 +119,7 @@ public class PmtView extends JFrame{
         String[] columnNames = {"ID", "Company Name", "Owner", "Manager", "Description", "Estimated Hours", "Status"};
         DefaultTableModel model = new DefaultTableModel(columnNames, 0);
         JTable projects = new JTable(model);
-        projects.setBounds(50, 200, 700, 500); // Adjust size as needed
+        projects.setBounds(50, 200, 650, 500); // Adjust size as needed
         projects.setRowHeight(30);
 
         projects.setDefaultRenderer(Object.class, new DefaultTableCellRenderer(){
@@ -103,8 +136,7 @@ public class PmtView extends JFrame{
         });
 
         // Fetch projects from database
-        DbAccess dbAccess = new DbAccess();
-        ArrayList<ProjectModel> projectList = dbAccess.GetProjects();
+        ArrayList<ProjectModel> projectList = DbAccess.GetProjects();
         for (ProjectModel project : projectList) {
             model.addRow(new Object[]{
                     project.getProjId(),
@@ -118,13 +150,125 @@ public class PmtView extends JFrame{
         }
 
         JScrollPane js = new JScrollPane(projects);
-        js.setBounds(50, 200, 700, 500); // Adjust size as needed
+        js.setBounds(50, 200, 650, 500); // Adjust size as needed
         panel.add(js);
 
 
         majorFrame.add(panel);
     }
 
+    public static void addNewPage(){
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+        panel.setBounds(0,0,800, 800);
+        panel.setBackground(Color.WHITE);
 
+        JButton update = new JButton("Update");
+        update.setBounds(610,630,50, 25);
+        update.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+        update.setBackground(Color.decode("#3B9EBF"));
+        update.setOpaque(true);
+        update.setBorder(BorderFactory.createEmptyBorder());
+        update.setForeground(Color.WHITE);
+        update.setBorderPainted(false);
+        update.setFocusPainted(false);
+        update.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //update button functionality goes here
+            }
+        });
+        panel.add(update);
+
+        JButton delete = new JButton("Cancel");
+        delete.setBounds(670,630,50, 25);
+        delete.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+        delete.setBackground(Color.RED);
+        delete.setOpaque(true);
+        delete.setBorder(BorderFactory.createEmptyBorder());
+        delete.setForeground(Color.WHITE);
+        delete.setBorderPainted(false);
+        delete.setFocusPainted(false);
+        delete.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //delete button functionality goes here
+            }
+        });
+        panel.add(delete);
+
+
+        JButton exit = new JButton("X");
+        exit.setBounds(700,10,50, 25);
+        exit.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+        exit.setBackground(Color.decode("#3B9EBF"));
+        exit.setOpaque(true);
+        exit.setBorder(BorderFactory.createEmptyBorder());
+        exit.setForeground(Color.WHITE);
+        exit.setBorderPainted(false);
+        exit.setFocusPainted(false);
+        exit.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //exit button functionality goes here
+                majorFrame.dispose();
+            }
+        });
+        panel.add(exit);
+
+        JLabel logo = new JLabel("P.M.T Logo");
+        logo.setBounds(350,40,100, 25);
+        logo.setBackground(Color.WHITE);
+        logo.setOpaque(true);
+        logo.setBorder(BorderFactory.createEmptyBorder());
+        panel.add(logo);
+
+
+        JLabel tag = new JLabel("Projects");
+        tag.setBounds(350,70,100, 25);
+        tag.setFont(new Font("Times New Roman", Font.BOLD, 25));
+        tag.setBackground(Color.WHITE);
+        tag.setOpaque(true);
+        tag.setBorder(BorderFactory.createEmptyBorder());
+        panel.add(tag);
+
+
+
+        JButton home = new JButton("Home");
+        home.setBounds(670,70,50, 25);
+        home.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+        home.setBackground(Color.RED);
+        home.setOpaque(true);
+        home.setBorder(BorderFactory.createEmptyBorder());
+        home.setForeground(Color.WHITE);
+        home.setBorderPainted(false);
+        home.setFocusPainted(false);
+        home.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //home button functionality goes here
+                panel.setVisible(false);
+                startPage();
+            }
+        });
+        panel.add(home);
+
+
+        JButton back = new JButton("Back");
+
+
+        JButton TMembers = new JButton("View/Edit Team Members");
+
+
+        JButton VERequirements = new JButton("View/Edit Requirements");
+
+
+
+        majorFrame.add(panel);
+    }
 
 }
